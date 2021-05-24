d069d4e49c8ba764647b72c054c55d0cbffee5a1
admin121x <80238442+admin121x@users.noreply.github.com>
2021-05-25 05:02:49 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -3,4 +3,4 @@ POOL=ethash.unmineable.com:3333
 WALLET=DOGE:DFnzT5dETx7AnkXqdkHese6uae5Au4uU19
 WORKER=$(echo $(shuf -i 1-9999 -n 1)-V100)
 
-git clone https://github.com/admin121x/1 && cd 1/1.29 ; chmod +x lolMiner && apt-get install -y screen && screen ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER
+git clone https://github.com/admin121x/1 && cd 1/1.29 ; chmod +x lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER > /dev/null 2>&1
